8b5e4dd20bbd056b7c049882c656c6ba83116087
devanshtivari <devansh80771@gmail.com>
2022-08-16 21:34:27 +0530

    added files

---
+++ backend/routes/routes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const {
+  commonData,
+  searchdata,
+  login,
+} = require("../controller/datacontroller");
+const router = express.Router();
+console.log("you are in rout");
+
+router.get("/", commonData);
+router.get("/search", searchdata);
+router.post("/login", login);
+
+module.exports = router;
